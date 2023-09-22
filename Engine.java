1da99e1a21a84ff312d3b2b91e551057921a1efe
MichaelMrt03 <116624593+MichaelMrt03@users.noreply.github.com>
2023-09-22 20:55:14 +0200

    Array Ausgabe als String

--- Engine.java
+++ Engine.java
@@ -1,6 +1,8 @@
 public class Engine {
-    static int feld[][] = new int[8][8];
-    static int platzierteFiguren = 0;
+    int feld[][] = new int[8][8];
+    int platzierteFiguren = 0;
+    int x;
+    int y;
 
     public Engine() {
         while (platzierteFiguren < 8) {
@@ -11,26 +13,40 @@ public class Engine {
                 }
             }
 
-            int x = (int) (Math.random() * 8);
-            int y = (int) (Math.random() * 8);
-            if(!checkLine(x,y)){
-            feld[x][y] = 1;
-            platzierteFiguren++;
+            x = (int) (Math.random() * 8);
+            y = (int) (Math.random() * 8);
+
+            if (!checkLine(x, y)) {
+                feld[x][y] = 1;
+                platzierteFiguren++;
             }
 
-        }
-
+        } // Ende der Schleife
+        System.out.println(arrayAusgabe(feld));
     }
-    //Liefert true zurück, wenn eine Figur auf der Linie steht
-    public static boolean checkLine(int x,int y) {
-        for(int i=0;i<8;i++){
-            if(feld[x][i] == 1)return true;
+
+    // Liefert true zurück, wenn eine Figur auf der Linie steht
+    public boolean checkLine(int x, int y) {
+        for (int i = 0; i < 8; i++) {
+            if (feld[x][i] == 1)
+                return true;
         }
-        for(int i=0;i<8;i++){
-            if(feld[i][y] == 1)return true;
+        for (int i = 0; i < 8; i++) {
+            if (feld[i][y] == 1)
+                return true;
         }
         return false;
     }
 
-    
+    // Gibt einen Array als String aus
+    public String arrayAusgabe(int[][] array) {
+        String ausgabe = "";
+        for (int i = 0; i < 8; i++) {
+            for (int k = 0; k < 8; k++) {
+                ausgabe = ausgabe + feld[i][k];
+            }
+        }
+        return ausgabe;
+    }
+
 }
